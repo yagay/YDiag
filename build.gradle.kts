4a85fe02cab44cc0657257853d9f627f27c0a297
Jie <1614094+yagay@users.noreply.github.com>
2026-09-25 15:36:08 +0800

    Add Android build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "9.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20" apply false
+}
